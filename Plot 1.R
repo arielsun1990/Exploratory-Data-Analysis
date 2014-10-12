53214c5bcb7f2eb3eab2f387b1403d34518bf2a3
SunWan <arielsun1990@gmail.com>
2014-10-13 05:14:54 +0800

    Plot 1

--- Plot 1.R
+++ Plot 1.R
@@ -1,4 +1,11 @@
-Exploratory-Data-Analysis
-=========================
-
-Project 1 of Exploratory Data Analysis
+rm(list=ls())
+#reading data
+read.table("household_power_consumption.txt", sep=";",nrows= 2075260, header=TRUE, quote= "", strip.white=TRUE, stringsAsFactors = FALSE, na.strings= "?")
+# Subsetting the full data to obtain the data related to two days
+subset(cons, (cons$Date == "1/2/2007" | cons$Date== "2/2/2007"))
+# Changing the class of Date variable from character to Date
+sub$Date <- as.Date(sub$Date, format = "%d/%m/%Y")
+# Creating the plot
+png("plot1.png", width = 480, height = 480)
+hist(sub$Global_active_power, type="l", main="Global Active Power",col='red',ylab= "Frequency", xlab="Global Active Power(kilowatts)")
+dev.off()
