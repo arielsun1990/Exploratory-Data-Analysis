d0955158cdfa3d530bd95748478e90f32702ed4b
SunWan <arielsun1990@gmail.com>
2014-10-13 05:31:16 +0800

    Update Plot 3.R

--- Plot 3.R
+++ Plot 3.R
@@ -10,6 +10,8 @@ powerConsumption2 <- powerConsumption[as.character(powerConsumption$Date) %in% c
 # Concatante Date and Time variables
 powerConsumption2$dateTime = paste(powerConsumption2$Date, powerConsumption2$Time)
 # Convert to Date/Time class
-powerConsumption2$dateTime <- strptime(powerConsumption2$dateTime, "%d/%m/%Y %H:%M:%S")attach(powerConsumption2)png("plot3.png", width=480, height=480, units="px")
+powerConsumption2$dateTime <- strptime(powerConsumption2$dateTime, "%d/%m/%Y %H:%M:%S")
+attach(powerConsumption2)
+png("plot3.png", width=480, height=480, units="px")
 # Plot Energy sub meteringplot(dateTime, as.numeric(as.character(Sub_metering_1)), type="l", xlab="", ylab="Energy sub metering", ylim=c(0,40))lines(dateTime, as.numeric(as.character(Sub_metering_2)), col="red")lines(dateTime, as.numeric(as.character(Sub_metering_3)), col="blue")legend("topright", lty=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ))
 dev.off()
