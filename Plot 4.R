242940bb011ff736f20daba3bce5be41ce773030
SunWan <arielsun1990@gmail.com>
2014-11-10 06:03:36 +0800

    Update Plot 4.R

--- Plot 4.R
+++ Plot 4.R
@@ -1,35 +1,39 @@
-# Change working directory
-setwd("EDA/Projects/Project1/dataCodes")
+library(data.table)
 
-# Url at which the data set is located
-fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-# Download zip file
-download.file(fileUrl, destfile="household_power_consumption.zip", method="curl")
-# Read data set after unzipping the data file
-powerConsumption <- read.table(unz("household_power_consumption.zip", "household_power_consumption.txt"),
-                               header=T, sep=";")
+# Read the original data
+DT <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 
-# Household power consumption for Feb. 1 and 2, 2007 only
-powerConsumption2 <- powerConsumption[as.character(powerConsumption$Date) %in% c("1/2/2007", "2/2/2007"),]
-# Concatante Date and Time variables
-powerConsumption2$dateTime = paste(powerConsumption2$Date, powerConsumption2$Time)
+# Convert the date
+DT$Date <- as.Date(DT$Date, format  = "%d/%m/%Y")
 
-# Convert to Date/Time class
-powerConsumption2$dateTime <- strptime(powerConsumption2$dateTime, "%d/%m/%Y %H:%M:%S")
-attach(powerConsumption2)
+# Subsetting by the date
+data <- subset(DT, Date >= as.Date("2007-2-1") & Date <= "2007-2-2")
+data$Global_active_power <- as.numeric(data$Global_active_power)
 
-png("plot4.png", width=480, height=480, units="px")
-# Plot Energy sub metering
-par(mfrow=c(2,2))
-plot(dateTime, as.numeric(as.character(Global_active_power)), type="l", xlab="", ylab="Global Active Power")
+rm(DT)
 
-plot(dateTime, as.numeric(as.character(Voltage)), type="l", xlab="datetime", ylab="Voltage")
+# Convert the time & date into weekday
+data$Datetime <- as.POSIXct(paste(data$Date, data$Time))
 
-plot(dateTime, as.numeric(as.character(Sub_metering_1)), type="l", xlab="", ylab="Energy sub metering", ylim=c(0,40))
-lines(dateTime, as.numeric(as.character(Sub_metering_2)), col="red")
-lines(dateTime, as.numeric(as.character(Sub_metering_3)), col="blue")
-legend("topright", lty=1, bty="n", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ))
+# Plotting
+par(mfcol = c(2,2), mar = c(4,4,1,4), oma = c(1,2,0,0))
 
-plot(dateTime, as.numeric(as.character(Global_reactive_power)), type="l", xlab="datetime", ylab="Global_reactive_power")
+plot(data$Global_active_power~data$Datetime, xlab = "", ylab = "Global Active Power", type = "l")
+
+with(data,
+{
+  plot(data$Sub_metering_1~data$Datetime, xlab = "", ylab = "Energy sub metering", type = "l", col = "black")
+  lines(data$Sub_metering_2~data$Datetime, type = "l", col = "red")
+  lines(data$Sub_metering_3~data$Datetime, type = "l", col = "blue")
+}
+)
+legend("topright", col = c("black", "red", "blue"), bty = "n", cex = 0.75, lty = 1, lwd = 1, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+plot(data$Voltage~data$Datetime, xlab = "datetime", ylab = "Voltage", type = "l")
+
+plot(data$Global_reactive_power~data$Datetime, xlab = "datetime", ylab = "Global_reactive_power", type = "l")
+
+# Save to png file
+dev.copy(png, file = "plot4.png", height = 480, width = 480)
 
 dev.off()
